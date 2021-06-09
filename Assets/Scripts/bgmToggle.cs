e45ef0cc1568c4f7c679420fefbb4b12d91fcb29
arvinchr <arvin.christopher27@gmail.com>
2021-06-10 00:00:45 +0700

    Add BGM
    Except Login and Register Scene

---
+++ Assets/Scripts/bgmToggle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class bgmToggle : MonoBehaviour
+{
+    public Sprite toggleOnSprite;
+    public Sprite toggleOffSprite;
+    private bool isToggled = false;
+    private Image image;
+    private AudioSource audio;
+    void Start()
+    {
+        image = GetComponent<Image>();
+        audio = GetComponent<AudioSource>();
+        UpdateSprite();
+    }
+
+    public void setBgmButton()
+    {
+        isToggled = !isToggled;
+        UpdateSprite();
+    }
+
+    void UpdateSprite()
+    {
+        if(isToggled)
+        {
+            image.sprite = toggleOnSprite;
+            audio.Play();
+        }
+        else
+        {
+            image.sprite = toggleOffSprite;
+            audio.Stop();
+        }
+    }
+}
